bed7346aaf92ab89b58eaba21440d0a94840b021
EnergoStalin <alex.vipers@mail.ru>
2022-07-29 02:47:52 +0300

    add multiple instances run guard

--- main.lua
+++ main.lua
@@ -2,20 +2,26 @@ local mp = require 'mp'
 local clip = require 'clipboard'
 local events = require 'events'
 
+local running = false
+
 mp.set_property('sub-delay', 0.5)
 
 local overlay = mp.create_osd_overlay('ass-events')
 events.display(overlay)
 
 mp.register_script_message("enable-sub-translator", function ()
+	if running then return end
+
 	mp.observe_property('sub-text', 'string', events.on_sub_changed)
 	mp.set_property('sub-visibility', 'no')
+	running = true
 end)
 
 mp.register_script_message("disable-sub-translator", function ()
 	mp.unobserve_property(on_sub_changed)
 	mp.set_property('sub-visibility', 'yes')
 	overlay:remove()
+	running = false
 end)
 
 mp.register_script_message("copy-sub-to-clipboard", function ()
