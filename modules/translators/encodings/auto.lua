2cacc42a8c2074504de1c77a7d02e78d0f43b7bd
EnergoStalin <alex.vipers@mail.ru>
2022-07-30 01:55:31 +0300

    move helper to another module

--- modules/translators/encodings/auto.lua
+++ modules/translators/encodings/auto.lua
@@ -1,12 +1,10 @@
+local path = (...):match("(.-)[^%.]+$")
+
 ---@param to string
 return function (to)
     if to:find("ru") then
-        return require 'modules.translators.encodings.cp1251'
+        return require (path .. 'cp1251')
     else
-        local aboba = {}
-        function aboba.to_utf8(value)
-            return value
-        end
-        return aboba
+        return require (path .. 'pass')
     end
 end
---
+++ modules/translators/encodings/pass.lua
@@ -0,0 +1,5 @@
+local aboba = {}
+function aboba.to_utf8(value)
+    return value
+end
+return aboba
