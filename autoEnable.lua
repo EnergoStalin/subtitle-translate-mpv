d73fc0875dd354a6e07b4bc8bece4a387b992cb2
EnergoStalin <alex.vipers@mail.ru>
2022-09-11 23:01:48 +0300

    fix autoenable after refactoring

--- autoEnable.lua
+++ autoEnable.lua
@@ -3,9 +3,14 @@ local tablelib = require 'tablelib'
 
 return function(register, unregister, options)
 	return function()
-		if #tablelib.filter(mp.get_property_native("track-list", {}), function (track)
-			return track.type == 'sub' and ((track.lang ~= nil and not track.lang:find(options.toLang)) or track.external)
-		end) ~= 0 then register()
+		local subs = tablelib.filter(mp.get_property_native("track-list", {}), function (track)
+			return track.type == 'sub'
+		end)
+		if #tablelib.filter(subs, function (track)
+			local val = track.lang ~= nil and track.lang:find(options.toLang)
+			mp.msg.info((track.lang or 'undefined') .. ' ' .. tostring(val))
+			return val
+		end) == 0 and #subs ~= 0 then register()
 		else unregister() end
 	end
 end
