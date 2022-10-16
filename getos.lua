10adcce52a25967ff73d79beec062de68be28919
EnergoStalin <alex.vipers@mail.ru>
2022-10-16 21:28:37 +0300

    fix linux encoding

---
+++ getos.lua
@@ -0,0 +1,14 @@
+-- Simple polyfill for os detection
+return function()
+    if jit and jit.os and jit.arch then
+        return jit.os
+    end
+
+
+    local popen_status, _ = pcall(io.popen, "")
+	if popen_status then
+        return "linux"
+    end
+
+    return "windows"
+end
--- modules/translators/encodings/auto.lua
+++ modules/translators/encodings/auto.lua
@@ -1,7 +1,12 @@
-local path = (...):match("(.-)[^%.]+$")
+local path = (...):match('(.-)[^%.]+$')
+local os = require ('getos')():lower()
 
 ---@param to string
 return function (to)
+	if os:find('linux') then
+		return require (path .. 'pass')
+	end
+
 	if to:find("ru") then
 		return require (path .. 'cp1251')
 	else
