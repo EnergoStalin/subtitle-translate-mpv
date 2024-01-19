26ef8311d861a6a004c7cd69c4137f67c7193b0f
Alexey Dashko <alex.vipers@mail.ru>
2024-01-19 06:29:35 +0300

    feat(crow): separate logic for linux & windows

--- modules/translators/crow.lua
+++ modules/translators/crow.lua
@@ -4,15 +4,37 @@ local auto = require 'modules.translators.encodings.auto'
 local getos = require 'getos'
 local tlib = require 'tablelib'
 
-local normalize = function(data)
-  return string.sub(data, 0, #data - 2)
+local normalize = function (data)
+	return string.sub(data, 0, #data - 2)
 end
 
-local os = getos()
-if os == 'linux' then
-  normalize = function(data)
-    return data
-  end
+local windowsInkove = function (commonArgs, value)
+	-- Removing '--' due to stupid crow CLI
+	-- Passing value as stdin_data not work on windows
+	local escaped = value:gsub('--', '')
+
+	table.insert(commonArgs, escaped)
+
+	local result = utils.subprocess({
+		args = commonArgs,
+		capture_stdout = true,
+		playback_only = true,
+	})
+
+	return result
+end
+
+local linuxInvoke = function (commonArgs, value)
+	table.insert(commonArgs, '-i')
+
+	local result = utils.subprocess({
+		args = commonArgs,
+		capture_stdout = true,
+		playback_only = true,
+		stdin_data = value,
+	})
+
+	return result
 end
 
 local postprocess = function (data)
@@ -20,47 +42,51 @@ local postprocess = function (data)
 	return text
 end
 
+local invoke = windowsInkove
+
+local os = getos():lower()
+if os:lower() == 'linux' then
+	normalize = function (data) return data end
+	invoke = linuxInvoke
+end
+
 ---@param from string
 ---@param to string
 return function (from, to)
-  local m = {}
-  local codepage = auto(to)
+	local m = {}
+	local codepage = auto(to)
 
-  ---@param value string
+	---@param value string
 	---@return string | nil
-  function m.translate(value)
-    -- Removing '--' due to stupid crow CLI
-		-- Passing value as stdin_data not work on windows
-		local escaped = value:gsub('--', '')
-		local args = {
+	function m.translate(value)
+		local commonArgs = {
 			'crow',
 			'-l', from,
 			'-t', to,
-			'-b', escaped
+			'-b'
 		}
 
-		local result = utils.subprocess({
-			args = args,
-			capture_stdout = true,
-			playback_only = true
-		})
+		local result = invoke(commonArgs, value)
 
 		if result.status ~= 0 then error(result) end
 
 		local data = codepage.to_utf8(result.stdout)
+
 		if data == nil then
-			mp.msg.warn('[crow] Got empty response ' .. tlib.join(args, ' '))
+			mp.msg.warn('[crow] Got empty response ' .. tlib.join(commonArgs, ' '))
 			return nil
 		end
 
 		return postprocess(data)
-  end
+	end
 
 	function m.get_error(err)
 		if err.status ~= nil and err.status == -3 then
 			return 'crow not reachable in path'
-		else return utils.format_json(err) end
+		else
+			return utils.format_json(err)
+		end
 	end
 
-  return m
+	return m
 end
