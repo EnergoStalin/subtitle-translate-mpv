419d1964c8216a70d8dd9b0755cca429732d043c
EnergoStalin <ens.stalin@gmail.com>
2024-05-19 12:32:42 +0300

    feat: always enable when sub-translator-debug passed

--- autoEnable.lua
+++ autoEnable.lua
@@ -2,8 +2,15 @@ local mp = require 'mp'
 local utils = require 'mp.utils'
 local tablelib = require 'tablelib'
 
+local debug = mp.get_opt('sub-translator-debug') ~= ''
+
 return function (register, unregister, options)
 	return function ()
+		if debug then
+			register()
+			return
+		end
+
 		local tracks = mp.get_property_native('track-list', {})
 		mp.msg.trace('[autoEnable] tracks', utils.format_json(tracks))
 		local subs = tablelib.filter(tracks, function (track)
