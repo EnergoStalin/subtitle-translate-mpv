b0a2717985eaa6acdfdb102ae282fd5e126e5f98
EnergoStalin <ens.stalin@gmail.com>
2024-05-19 12:32:13 +0300

    fix(nvim): cwd

--- .nvim.lua
+++ .nvim.lua
@@ -6,8 +6,8 @@ overseer.register_template({
     return {
       name = 'Debug subtitle-translator',
       cmd = 'mpv --fs --fs-screen=0 --slang=en --script-opts-append=sub-translator-debug=1 --start=00:00:08 --msg-level=subtitle_translate_mpv=trace,osd=debug --profile=infinite test.*',
-      cwd = vim.fn.resolve(vim.fn.getcwd() .. '/scripts/subtitle-translate-mpv'),
-      components = {'unique', 'default'}
+      components = {'unique', 'default'},
+      cwd = vim.fn.getcwd()
     }
   end,
 })
