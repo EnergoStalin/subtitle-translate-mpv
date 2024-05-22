02c420e6404e30da58b3ac29a660604770bc6a9a
EnergoStalin <ens.stalin@gmail.com>
2024-05-23 02:38:54 +0300

    chore: move options in dedicated file

--- main.lua
+++ main.lua
@@ -1,27 +1,5 @@
 local mp = require 'mp'
-local opt = require 'mp.options'
-
-local o = {
-	defaultDelay = -0.5,
-	translator = 'Console-Translate',
-	autoEnableTranslator = false,
-	translatedOnly = false,
-	primaryOriginal = false,
-	fromLang = 'en',
-	toLang = 'ru',
-	osdFont = mp.get_property('osd-font'),
-	osdFontSize = mp.get_property('osd-font-size'),
-	osdOriginalFontScale = 50,
-	overrideFonts = true,
-	sensitivity = 8,
-
-	-- Mega Cringe
-	running = false,
-	disabledManually = false,
-	userDelay = 0,
-	actualDelay = 0,
-}
-opt.read_options(o, mp.get_script_name())
+local o = require 'options'
 
 local overlay = require 'overlay' (o)
 local provider = require('modules.translators.' .. o.translator)
