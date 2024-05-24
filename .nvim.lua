836968e4a412194330d361b0f2bbaa44f16a9234
EnergoStalin <ens.stalin@gmail.com>
2024-05-24 18:04:13 +0300

    chore(nvim): extract subtitle

--- .nvim.lua
+++ .nvim.lua
@@ -1,22 +1,42 @@
 local overseer = require('overseer')
 
 overseer.register_template({
-	name = 'Debug subtitle-translator',
+	name = 'debug',
 	builder = function ()
 		return {
-			name = 'Debug subtitle-translator',
+			name = 'subtitle-translator',
 			cmd = {
 				'mpv',
 				'--fs',
 				'--fs-screen=0',
 				'--slang=en',
 				'--script-opts-append=sub-translator-debug=1',
-				'--start=00:00:08',
+				'--start=00:00:01',
 				'--msg-level=subtitle_translate_mpv=trace,osd=debug',
 				'--profile=infinite',
 				'test.mkv'
 			},
 			components = { 'unique', 'default', },
+			cwd = vim.fn.getcwd(),
+		}
+	end,
+})
+
+overseer.register_template({
+	name = 'ffmpeg extract subtitles',
+	builder = function ()
+		return {
+			name = 'ffmpeg',
+			cmd = {
+				'ffmpeg',
+				'-i',
+				'test.mkv',
+				'-y',
+				'-map',
+				'0:s:0',
+				'test.ass',
+			},
+			components = { 'unique', 'default' },
 			cwd = vim.fn.getcwd(),
 		}
 	end,
